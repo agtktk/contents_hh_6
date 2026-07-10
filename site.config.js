492e50ac40ec19a4b0b61a4c85e319899a9f5e6c
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:53:47 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: '콘텐츠이용료미납 희망티켓',
+  siteUrl: 'https://contents-hh-6.pages.dev',
+  siteDescription: '콘텐츠이용료미납·연체 정책 불가 전문 해결. 막힌 결제 한도도 24시간 맞춤형 상담을 통해 신속히 도와드립니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
