6bac5ae38f0fc0256d2ff2ad267fbdcfd2bf2e05
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:49:49 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: '콘텐츠이용료미납·연체 정책 불가 전문 해결. 막힌 결제 한도도 24시간 맞춤형 상담을 통해 신속히 도와드립니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: '0a440da24b1f4cce7aec0f403f877ea204314fe9',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
